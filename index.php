4d68932519ab861f7e033735bd97e6ef231af0e2
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-20 16:48:27 +0100

    importo i data dei dischi

--- index.php
+++ index.php
@@ -8,16 +8,29 @@
     <!-- BootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-
+    <!-- Vue js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
 <body>
     
+    <div id="app">
+
+        <div class="container">
+            
+        </div>
+
+    </div>
+
+
 
 
 
 
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- my script -->
+    <script src="./js/main.js"></script>
 
 </body>
 </html>
