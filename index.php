a4233d5d8210dd1694b0a1854ab6d407ad75f4af
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-20 18:54:29 +0100

    creo ogni card per ogni oggetto dell'array

--- index.php
+++ index.php
@@ -21,7 +21,19 @@
         <div class="container">
             <h1>ciao</h1>
 
-            
+            <div class="row g-3 ">
+                <div v-for="disk in disksList" class="card" style="width: 18rem;">
+                    <img :src="disk.poster" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                    </div>
+                    
+                </div>
+
+            </div>
+
         </div>
 
     </div>
