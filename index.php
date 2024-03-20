2dda85f02c74afd532f99ec24f78af3e97482442
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-20 19:08:31 +0100

    stilizzazione finita

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <!-- BootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- my style -->
+    <link rel="stylesheet" href="./css/style.css" />
+    
     <!-- Vue js -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
@@ -17,17 +20,21 @@
 <body>
     
     <div id="app">
+        
+        <header>
+            
+        </header>
 
         <div class="container">
 
             <div class="row">
                 <div v-for="disk in disksList" class="col-4 g-5">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card my-card-bg" style="width: 18rem;">
                         <img :src="disk.poster" class="card-img-top p-3" :alt="disk.title">
                         <div class="card-body p-3 text-center">
                             <h5 class="card-title">{{disk.title}}</h5>
                             <p class="card-text">{{disk.author}}</p>
-                            <h5 class="card-subtitle mb-2 text-body-secondary">{{disk.year}}</h5>
+                            <h5 class="card-subtitle mb-2">{{disk.year}}</h5>
                         </div>
                     </div>
                 </div>
